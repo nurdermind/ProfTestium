bfe632caa456ed1f107217d75fcccb68376a1161
tm.kk23 <mansur.tumashev@gmail.com>
2023-10-27 23:39:59 +0500

    restruct

--- web/ProfTestium/settings.py
+++ web/ProfTestium/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ProfTestium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'web/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
