a7418eb9c874c7d485b95abc3da59800b35c92f5
tm.kk23 <mansur.tumashev@gmail.com>
2023-10-28 11:26:08 +0500

    create app tests_app

---
+++ web/backend/tests_app/serializers/sessions_serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+
+class TestsSenderReplySerializer(serializers.Serializer):
+    TestID = serializers.IntegerField()
+    TestName = serializers.CharField()
+
+
+class SessionUserSerializer(serializers.Serializer):
+    SessionID = serializers.IntegerField()
+    testssenderReply = TestsSenderReplySerializer()
+
+    SessionDate = serializers.DateTimeField()
+    DurationSession = serializers.DurationField()
+
+    IsSuccessful = serializers.BooleanField()
+    FailureReason = serializers.CharField()
+    SessionUrl = serializers.URLField()
+    Score = serializers.IntegerField()
+    MaxScore = serializers.IntegerField()
